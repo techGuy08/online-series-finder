b6c2afb30cce35d151d4987fb8dd4300b4dc0fe8
korro21 <42660934+korro21@users.noreply.github.com>
2020-04-17 20:21:19 +0100

    Updates

---
+++ precache-manifest.d23da5687487b7ed132c0a2a5ae7b45e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8778efa477d55d4fe03",
+    "url": "/online-series-finder/static/css/main.9508a2fb.chunk.css"
+  },
+  {
+    "revision": "f8778efa477d55d4fe03",
+    "url": "/online-series-finder/static/js/main.f8778efa.chunk.js"
+  },
+  {
+    "revision": "cb28dc334b95a10333eb",
+    "url": "/online-series-finder/static/js/1.cb28dc33.chunk.js"
+  },
+  {
+    "revision": "73e1aae604e610beee41",
+    "url": "/online-series-finder/static/js/runtime~main.73e1aae6.js"
+  },
+  {
+    "revision": "c821683c573bd9b748466f877909821a",
+    "url": "/online-series-finder/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/online-series-finder/precache-manifest.d23da5687487b7ed132c0a2a5ae7b45e.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/online-series-finder/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
